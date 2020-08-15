288533d030342116e0b470a9385291f099a825dc
ibfahd <ibfahd@gmail.com>
2020-08-15 19:02:18 +0100

    Done

--- Hash Tables Ransom Note.py
+++ Hash Tables Ransom Note.py
@@ -4,21 +4,23 @@ import os
 import random
 import re
 import sys
+from collections import Counter
 # Complete the checkMagazine function below.
+
 def checkMagazine(magazine, note):
+    cm = Counter(magazine)
+    cn = Counter(note)
     res = True
-    for n in note:
-        if n in magazine:
-            cnn = note.count(n)
-            cnm = magazine.count(n)
-            res = res and (cnn <= cnm)
+    for n in cn:
+        if n in cm:
+            res = res and (cm[n] >= cn[n])
         else:
             res = False
     if res:
         print("Yes")
     else:
         print("No")
-        
+
 if __name__ == '__main__':
     mn = input().split()
     m = int(mn[0])
